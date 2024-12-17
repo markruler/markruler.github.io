cb78769e395265fb845cc437f6382ea0c9814e93
Changsu Im <imcxsu@gmail.com>
2024-12-18 00:18:52 +0900

    rebuilding site 12/18/2024 00:18:52 on Windows

--- scripts/windows.deploy.ps1
+++ scripts/windows.deploy.ps1
@@ -1,5 +1,8 @@
 Write-Host "Deploying updates to GitHub..." -ForegroundColor Magenta
 
+# Get-Host | Select-Object Version
+Get-Host
+
 git add -A
 git commit -m "rebuilding site $(Get-Date) on Windows"
 git push origin HEAD
